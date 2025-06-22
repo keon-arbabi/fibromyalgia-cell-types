0dac82db9199ca68fffd3f4750bff334202af31a
keon_arbabi <keon.arbabi@gmail.com>
2025-06-22 04:05:59 -0400

    update

--- download_mouse_brain.py
+++ download_mouse_brain.py
@@ -1,17 +1,18 @@
-import os, anndata
+import os
+import pandas as pd
+import scanpy as sc
 import matplotlib.pyplot as plt
-
 from pathlib import Path
 from abc_atlas_access.abc_atlas_cache.abc_project_cache import AbcProjectCache
 
 # https://github.com/AllenInstitute/abc_atlas_access/blob/main/notebooks/getting_started.ipynb
 # https://github.com/AllenInstitute/abc_atlas_access/blob/main/notebooks/zhuang_merfish_tutorial.ipynb
 
-download_base = Path('projects/rrg-wainberg/karbabi/single-cell/ABC')
+#region download data #########################################################
+
+download_base = Path('projects/rrg-wainberg/single-cell/ABC')
 abc_cache = AbcProjectCache.from_s3_cache(download_base)
 abc_cache.current_manifest
-
-# cell meta data ###############################################################
 
 datasets = ['Zhuang-ABCA-1', 'Zhuang-ABCA-3']
 
@@ -77,13 +78,25 @@ for d in datasets :
 
 for d in datasets:
     cell_extended[d].to_csv(
-        'projects/def-wainberg/spatial/Zhuang/direct-downloads/'
-        f'{d}-metadata.csv')
-    
-# plot slices ##################################################################
+        f'{download_base}/metadata/{d}-metadata.csv')
 
-figure_dir = 'projects/def-wainberg/karbabi/spatial-pregnancy-postpart/figures'
-os.makedirs(f'{figure_dir}/Zhuang_ref', exist_ok=True)
+# download anndata objects (run once); must be online
+abc_cache.get_data_path(directory='Zhuang-ABCA-1', file_name='raw') 
+abc_cache.get_data_path(directory='Zhuang-ABCA-3', file_name='raw')
+
+#endregion
+
+#region plot slices ############################################################
+
+download_base = Path('projects/rrg-wainberg/single-cell/ABC')
+
+cell_extended = {}
+for d in ['Zhuang-ABCA-1', 'Zhuang-ABCA-3']:
+    cell_extended[d] = pd.read_csv(
+        f'{download_base}/metadata/{d}-metadata.csv')
+
+figure_dir = f'{download_base}/figures'
+os.makedirs(figure_dir, exist_ok=True)
 
 def subplot_section(ax, xx, yy, cc=None, val=None, cmap=None):
     if cmap is not None:
@@ -98,7 +111,6 @@ def subplot_section(ax, xx, yy, cc=None, val=None, cmap=None):
 
 def plot_sections(cell_extended, dataset, sections, 
                   cc=None, val=None, cmap=None):
-    # Parse the floating point numbers and sort the sections
     sorted_sections = sorted(sections, key=lambda x: float(x.split('.')[-1]))
     n_sections = len(sorted_sections)
     n_plots = (n_sections + 7) // 8  
@@ -134,6 +146,40 @@ for dataset in cell_extended.keys():
     plots = plot_sections(cell_extended, dataset, sections, 'class_color')
     for idx, fig in enumerate(plots, 1):
         fig.suptitle(f'{dataset}', fontsize=14)
-        fig.savefig(f'{figure_dir}/reference/zhuang_ref/{dataset}_{idx}.png',
-                    dpi=300)  
+        fig.savefig(f'{figure_dir}/{dataset}_{idx}.png', dpi=300)  
         plt.close(fig)  
+
+#endregion
+
+#region preprocess data #######################################################
+
+dataset = 'Zhuang-ABCA-1'
+download_base = Path('projects/rrg-wainberg/single-cell/ABC')
+
+samples = {
+    'Zhuang-ABCA-1': ['Zhuang-ABCA-1.078'],
+    'Zhuang-ABCA-3': ['Zhuang-ABCA-3.004']
+}
+
+for dataset in samples.keys():
+    sample = samples[dataset]
+    metadata = pd.read_csv(
+        f'{download_base}/metadata/{dataset}-metadata.csv',
+        index_col='cell_label')
+    adata = sc.read_h5ad(
+        f'{download_base}/expression_matrices/{dataset}/20230830/'
+        f'{dataset}-raw.h5ad')
+    adata = adata[adata.obs.index.isin(metadata.index)].copy()
+    adata = adata[adata.obs['brain_section_label'].isin(sample)].copy()
+    adata.obs['sample'] = adata.obs['brain_section_label']
+    adata.obs['source'] = 'Zhuang-ABCA-Reference'
+    adata.obs = adata.obs.join(
+        metadata, on='cell_label', lsuffix='_l', rsuffix='')
+    adata.obs['y'] = -adata.obs['y']
+    adata.var.reset_index()
+    adata.var.index = adata.var['gene_symbol']
+    adata.layers['count'] = adata.X.copy()
+    adata.obsm['spatial'] = adata.obs[['x', 'y']].to_numpy()
+    adata.write(f'{download_base}/anndata/{dataset}-raw.h5ad')
+
+#endregion
--- run_gsmap.py
+++ run_gsmap.py
@@ -1,13 +1,20 @@
 import os
 import gzip
 import polars as pl
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import seaborn as sns
-import random
+import scanpy as sc
 from utils import run, p_to_abs_z, read_csv_delim_whitespace
 
+plt.rcParams['svg.fonttype'] = 'none'
+plt.rcParams['font.family'] = 'DejaVu Sans'
+plt.rcParams['figure.dpi'] = 500
+
 workdir = 'projects/rrg-wainberg/karbabi/fibromyalgia_cell_types'
+
+#region prep data ##############################################################
 
 if not os.path.exists(f'{workdir}/gsMap_resource'):
     run(f'wget https://yanglab.westlake.edu.cn/data/gsMap/gsMap_resource.tar.gz '
@@ -49,6 +56,10 @@ if not os.path.exists(formatted_gwas_file):
     csv_string = formatted_df.write_csv(separator='\t')
     with gzip.open(formatted_gwas_file, "wt") as f:
         f.write(csv_string)
+
+#endregion
+
+#region mouse embryo ##########################################################
 
 run(f'''
     gsmap quick_mode \
@@ -110,7 +121,7 @@ if os.path.exists(gsmap_plot_file) and os.path.exists(cauchy_results_file):
         .with_columns((-pl.col('p_cauchy').log10())
                       .alias('-log10(p_cauchy)'))
 
-    fig = plt.figure(figsize=(22, 8))
+    fig = plt.figure(figsize=(20, 8))
     fig.set_facecolor('white')
     gs_main = gridspec.GridSpec(
         1, 2, width_ratios=[2, 0.6], wspace=0.1, figure=fig
@@ -122,40 +133,43 @@ if os.path.exists(gsmap_plot_file) and os.path.exists(cauchy_results_file):
     ax2 = fig.add_subplot(gs_spatial[1])
     ax3 = fig.add_subplot(gs_main[1])
 
+    annos = sorted(df['annotation'].unique())
+    color_map = {anno: color_dict[anno] for anno in annos}
     sns.scatterplot(
-        data=df, x='sx', y='sy', hue='logp', palette='magma',
-        s=5, linewidth=0, ax=ax1, legend=False
+        data=df, x='sx', y='sy', hue='annotation', hue_order=annos,
+        palette=color_map, s=5, linewidth=0, ax=ax1, legend=False,
+        rasterized=True
     )
     ax1.set_facecolor('black')
     ax1.set_aspect('equal', adjustable='box')
     ax1.axis('off')
-    ax1.set_title(trait_name, color='black', fontsize=16)
+    ax1.set_title('E16.5', color='black', fontsize=16)
+
+    sns.scatterplot(
+        data=df, x='sx', y='sy', hue='logp', palette='magma',
+        s=5, linewidth=0, ax=ax2, legend=False,
+        rasterized=True
+    )
+    ax2.set_facecolor('black')
+    ax2.set_aspect('equal', adjustable='box')
+    ax2.axis('off')
+    ax2.set_title(trait_name, color='black', fontsize=16)
 
     norm = plt.Normalize(df['logp'].min(), df['logp'].max())
     sm = plt.cm.ScalarMappable(cmap="magma", norm=norm)
     sm.set_array([])
-    cbar_ax = fig.add_axes([0.13, 0.08, 0.15, 0.02])
+    cbar_ax = fig.add_axes([0.33, 0.08, 0.15, 0.02])
     cbar = fig.colorbar(sm, cax=cbar_ax, orientation='horizontal')
     cbar.set_label(r'$-\log_{10}(\mathrm{P\ value})$', color='black', size=12)
     cbar.ax.tick_params(colors='black')
     cbar.outline.set_edgecolor('black')
 
-    annos = sorted(df['annotation'].unique())
-    color_map = {anno: color_dict[anno] for anno in annos}
-    sns.scatterplot(
-        data=df, x='sx', y='sy', hue='annotation', hue_order=annos,
-        palette=color_map, s=5, linewidth=0, ax=ax2, legend=False
-    )
-    ax2.set_facecolor('black')
-    ax2.set_aspect('equal', adjustable='box')
-    ax2.axis('off')
-    ax2.set_title('E16.5', color='black', fontsize=16)
-
     plot_df_pd = plot_df.to_pandas()
     sns.barplot(
         data=plot_df_pd, x='-log10(p_cauchy)', y='annotation',
         palette=color_map, ax=ax3, hue='annotation', dodge=False,
-        legend=False, order=plot_df_pd['annotation']
+        legend=False, order=plot_df_pd['annotation'],
+        rasterized=True
     )
     ax3.set_title(f'gsMap Cauchy P-values for {trait_name}')
     ax3.set_xlabel('-log10(p-value)')
@@ -167,3 +181,169 @@ if os.path.exists(gsmap_plot_file) and os.path.exists(cauchy_results_file):
                 f'{sample_name}_{trait_name}_spatial_combined.png')
     plt.savefig(path_svg, bbox_inches='tight', pad_inches=0.1)
     plt.savefig(path_png, bbox_inches='tight', pad_inches=0.1)
+    plt.close(fig)
+
+#endregion
+
+#region mouse brain ###########################################################
+
+run(f'''
+    gsmap quick_mode \
+    --workdir '{workdir}/out' \
+    --homolog_file '{workdir}/gsMap_resource/homologs/mouse_human_homologs.txt' \
+    --sample_name 'Zhuang-ABCA-1-raw' \
+    --gsMap_resource_dir '{workdir}/gsMap_resource' \
+    --hdf5_path '{workdir}/gsMap_example_data/ST/Zhuang-ABCA-1-raw.h5ad' \
+    --annotation 'parcellation_division' \
+    --data_layer 'count' \
+    --sumstats_file '{formatted_gwas_file}' \
+    --trait_name 'Fibromyalgia'
+''')
+
+run(f'''
+    gsmap quick_mode \
+    --workdir '{workdir}/out' \
+    --homolog_file '{workdir}/gsMap_resource/homologs/mouse_human_homologs.txt' \
+    --sample_name 'Zhuang-ABCA-3-raw' \
+    --gsMap_resource_dir '{workdir}/gsMap_resource' \
+    --hdf5_path '{workdir}/gsMap_example_data/ST/Zhuang-ABCA-3-raw.h5ad' \
+    --annotation 'parcellation_division' \
+    --data_layer 'count' \
+    --sumstats_file '{formatted_gwas_file}' \
+    --trait_name 'Fibromyalgia'
+''')
+
+
+adata_1 = sc.read_h5ad(f'{workdir}/gsMap_example_data/ST/Zhuang-ABCA-1-raw.h5ad')
+adata_3 = sc.read_h5ad(f'{workdir}/gsMap_example_data/ST/Zhuang-ABCA-3-raw.h5ad')
+
+adata = sc.concat([adata_1, adata_3])
+
+color_df = adata.obs[['class', 'class_color']].drop_duplicates()
+brain_color_map = {
+    row['class']: row['class_color'] for _, row in color_df.iterrows()
+}
+
+parc_color_df = adata.obs[['parcellation_division',
+                          'parcellation_division_color']].drop_duplicates()
+parc_color_map = {
+    row['parcellation_division']: row['parcellation_division_color']
+    for _, row in parc_color_df.iterrows()
+}
+
+sample_sizes = {'Zhuang-ABCA-1-raw': 3, 'Zhuang-ABCA-3-raw': 0.25}
+
+fig = plt.figure(figsize=(30, 12))
+fig.set_facecolor('white')
+gs_main = gridspec.GridSpec(
+    2, 5, height_ratios=[1, 1], width_ratios=[1, 1, 1, 0.8, 0.8],
+    wspace=0.2, hspace=0.3, figure=fig
+)
+
+for i, sample_name in enumerate(['Zhuang-ABCA-1-raw', 'Zhuang-ABCA-3-raw']):
+    trait_name = 'Fibromyalgia'
+
+    adata_sample = sc.read_h5ad(
+        f'{workdir}/gsMap_example_data/ST/{sample_name}.h5ad'
+    )
+    spatial_df = pd.DataFrame(
+        adata_sample.obsm['spatial'],
+        columns=['sx', 'sy'],
+        index=adata_sample.obs.index
+    )
+    plot_data = adata_sample.obs.join(spatial_df)
+
+    gsmap_plot_file = (
+        f'{workdir}/out/{sample_name}/report/{trait_name}/gsMap_plot/'
+        f'{sample_name}_{trait_name}_gsMap_plot.csv'
+    )
+    logp_df = pl.read_csv(
+        gsmap_plot_file, schema_overrides={'': pl.Utf8}
+    ).to_pandas().set_index('')
+    plot_data = plot_data.join(logp_df[['logp']])
+
+    class_cauchy_file = (
+        f'{workdir}/out/{sample_name}/cauchy_combination/'
+        f'{sample_name}_{trait_name}.Class.Cauchy.csv.gz'
+    )
+    parc_cauchy_file = (
+        f'{workdir}/out/{sample_name}/cauchy_combination/'
+        f'{sample_name}_{trait_name}.Parcellation.Cauchy.csv.gz'
+    )
+    class_plot_df = pl.read_csv(class_cauchy_file).sort('p_cauchy')\
+        .with_columns((-pl.col('p_cauchy').log10()).alias('-log10(p_cauchy)'))
+    parc_plot_df = pl.read_csv(parc_cauchy_file).sort('p_cauchy')\
+        .with_columns((-pl.col('p_cauchy').log10()).alias('-log10(p_cauchy)'))
+
+    ax1 = fig.add_subplot(gs_main[i, 0])
+    ax2 = fig.add_subplot(gs_main[i, 1])
+    ax3 = fig.add_subplot(gs_main[i, 2])
+    ax4 = fig.add_subplot(gs_main[i, 3])
+    ax5 = fig.add_subplot(gs_main[i, 4])
+
+    sns.scatterplot(
+        data=plot_data, x='sx', y='sy', hue='class',
+        palette=brain_color_map, s=sample_sizes[sample_name], linewidth=0,
+        ax=ax1, legend=False, rasterized=True,
+        hue_order=sorted(brain_color_map.keys())
+    )
+    ax1.set_facecolor('black')
+    ax1.set_aspect('equal')
+    ax1.axis('off')
+    ax1.set_title(f'{sample_name} - Class', y=1.05)
+
+    sns.scatterplot(
+        data=plot_data, x='sx', y='sy', hue='parcellation_division',
+        palette=parc_color_map, s=sample_sizes[sample_name], linewidth=0,
+        ax=ax2, legend=False, rasterized=True,
+        hue_order=sorted(parc_color_map.keys())
+    )
+    ax2.set_facecolor('black')
+    ax2.set_aspect('equal')
+    ax2.axis('off')
+    ax2.set_title(f'{sample_name} - Parcellation', y=1.05)
+
+    sns.scatterplot(
+        data=plot_data, x='sx', y='sy', hue='logp', palette='magma',
+        s=sample_sizes[sample_name], linewidth=0, ax=ax3, legend=False,
+        rasterized=True
+    )
+    ax3.set_facecolor('black')
+    ax3.set_aspect('equal')
+    ax3.axis('off')
+    ax3.set_title(trait_name, y=1.05)
+    norm = plt.Normalize(plot_data['logp'].min(), plot_data['logp'].max())
+    sm = plt.cm.ScalarMappable(cmap="magma", norm=norm)
+    sm.set_array([])
+    cbar_ax_bbox = ax3.get_position()
+    cbar_ax = fig.add_axes(
+        [cbar_ax_bbox.x0, cbar_ax_bbox.y0 - 0.05, cbar_ax_bbox.width, 0.02]
+    )
+    cbar = fig.colorbar(sm, cax=cbar_ax, orientation='horizontal')
+    cbar.set_label(r'$-\log_{10}(\mathrm{P\ value})$')
+
+    class_plot_df_pd = class_plot_df.to_pandas()
+    sns.barplot(
+        data=class_plot_df_pd, x='-log10(p_cauchy)', y='annotation',
+        hue='annotation', legend=False, palette=brain_color_map, ax=ax4,
+        dodge=False, order=class_plot_df_pd['annotation'], rasterized=True
+    )
+    ax4.set_title('Class P-values')
+    ax4.set_xlabel('')
+    ax4.set_ylabel('')
+
+    parc_plot_df_pd = parc_plot_df.to_pandas()
+    sns.barplot(
+        data=parc_plot_df_pd, x='-log10(p_cauchy)', y='annotation',
+        hue='annotation', legend=False, palette=parc_color_map, ax=ax5,
+        dodge=False, order=parc_plot_df_pd['annotation'], rasterized=True
+    )
+    ax5.set_title('Parcellation P-values')
+    ax5.set_xlabel('')
+    ax5.set_ylabel('')
+
+path_svg = f'{workdir}/figures/mouse_brain_{trait_name}_spatial_combined.svg'
+path_png = f'{workdir}/figures/mouse_brain_{trait_name}_spatial_combined.png'
+plt.savefig(path_svg, bbox_inches='tight', pad_inches=0.1)
+plt.savefig(path_png, bbox_inches='tight', pad_inches=0.1)
+plt.close(fig)
